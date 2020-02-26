8332c66f99b81b0975858b426405155c296d162a
xavierThufail <xavier.thufail@gmail.com>
2020-02-26 22:46:17 +0700

    Commit repair

--- Routes/recommend.js
+++
@@ -1,6 +0,0 @@
-const router = require('express').Router()
-const Controller = require('../controllers/RecommendController')
-
-router.get('/', (req, res) => res.render('recommend', {isLogin: false}))
-
-module.exports = router
--- routes/store.js
+++
@@ -1,6 +0,0 @@
-const router = require('express').Router()
-const Controller = require('../controllers/StoreController')
-
-router.get('/', (req, res) => res.render('store', {isLogin: false}))
-
-module.exports = router
